fbfe30623085003f046794679e94aceb1cae507a
Raschid JF Rafaelly <me@raschidjfr.dev>
2025-12-07 15:44:39 -0500

    fix(auth): add host to trustedOrigins

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -7,6 +7,11 @@ const mongoUri = process.env.MONGODB_URI!;
 const dbName = process.env.DATABASE_NAME;
 const client = new MongoClient(mongoUri);
 const db = client.db(dbName);
+const trustedOrigins = process.env.APP_URL
+  ? [process.env.APP_URL]
+  : process.env.NODE_ENV === 'development'
+  ? ['http://localhost:3000']
+  : [];
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, { client }),
@@ -16,7 +21,7 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
-  trustedOrigins: [process.env.APP_URL || 'http://localhost:3000'],
+  trustedOrigins,
   appName: 'Bento Mail',
   basePath: '/api/auth',
   user: {
